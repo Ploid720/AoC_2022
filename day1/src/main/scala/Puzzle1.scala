aaefa3746f2f7c358a2543f073585cdc1fcce332
Michał Kullass <mkull@onet.pl>
2022-12-01 17:57:38 +0100

    Day 1

---
+++ day1/src/main/scala/Puzzle1.scala
@@ -0,0 +1,17 @@
+object Puzzle1
+{
+    def main(args: Array[String])
+    {
+        println(io.Source
+            .fromResource("input.txt")
+            .getLines
+            .toList
+            .foldLeft(List[Int]())((acc, el) => (acc, el) match
+            {
+                case (l, s) if s.trim.isEmpty => 0 :: l
+                case (h :: t, s) => (h + s.toInt) :: t
+                case (_, s) => List(s.toInt)
+            })
+            .max)
+    }
+}
