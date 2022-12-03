f0e81abf7c7bd124662ac28bc5751dc2d8aa941b
Michał Kullass <mkull@onet.pl>
2022-12-03 17:21:17 +0100

    Day 3

---
+++ day3/src/main/scala/Puzzle1.scala
@@ -0,0 +1,19 @@
+object Puzzle1
+{
+    def main(args: Array[String])
+    {
+        println(io.Source
+            .fromResource("input.txt")
+            .getLines
+            .toList
+            .map(s => (s, s.length / 2))
+            .map(t => (t._1.take(t._2), t._1.drop(t._2).toSet))
+            .map(t => t._1.find(e => t._2.contains(e)).get)
+            .map(_ match
+            {
+                case c if c.isUpper => c - 'A' + 27
+                case c => c - 'a' + 1
+            })
+            .sum)
+    }
+}
---
+++ day3/src/main/scala/Puzzle2.scala
@@ -0,0 +1,23 @@
+object Puzzle2
+{
+    def main(args: Array[String])
+    {
+        println(io.Source
+            .fromResource("input.txt")
+            .getLines
+            .toList
+            .grouped(3)
+            .map(_ match
+            {
+                case List(a, b, c) => (a, b.toSet, c.toSet)
+                case _ => throw new IllegalArgumentException
+            })
+            .map(t => t._1.find(e => t._2.contains(e) && t._3.contains(e)).get)
+            .map(_ match
+            {
+                case c if c.isUpper => c - 'A' + 27
+                case c => c - 'a' + 1
+            })
+            .sum)
+    }
+}
