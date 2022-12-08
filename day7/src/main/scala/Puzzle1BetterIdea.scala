f1dbea3e0608518d176d3f46e84948f2123ffeb1
Michał Kullass <mkull@onet.pl>
2022-12-08 19:22:55 +0100

    Day 7, better idea

---
+++ day7/src/main/scala/Puzzle1BetterIdea.scala
@@ -0,0 +1,73 @@
+import scala.annotation.tailrec
+import collection.immutable.Queue
+
+object Puzzle1BetterIdea
+{
+    trait InElem
+    {}
+    case class File(name: String, size: Int) extends InElem
+    {}
+    case class ChangeDirectory(to: String) extends InElem
+    {}
+
+    def calculateDirSizes(data: List[InElem]): Map[String, Int] =
+    {
+        @tailrec
+        def scanData(left: List[InElem], path: List[String],
+            dirsizes: Map[String, Int]): Map[String, Int] =
+        {
+            left match {
+                case File(name, size) :: t => scanData(t, path, updateSizeMap(size, dirsizes, path))
+                case ChangeDirectory(to) :: t => to match {
+                    case "/" => scanData(t, List("/"), dirsizes)
+                    case ".." => scanData(t, path.tail, dirsizes)
+                    case _ => scanData(t, to :: path, dirsizes)
+                }
+                case _ => dirsizes
+            }
+        }
+        @tailrec
+        def updateSizeMap(size: Int, map: Map[String, Int], dirs: List[String]): Map[String, Int] =
+        {
+            dirs match {
+                case h :: t  => {
+                    val absName = dirs.mkString("/")
+                    if (map.contains(absName))
+                        updateSizeMap(size, map + (absName -> (map(absName) + size)), t)
+                    else
+                        updateSizeMap(size, map + (absName -> size), t)
+                }
+                case _ => map
+            }
+        }
+
+        scanData(data, List("/"), Map())
+    }
+
+    def main(args: Array[String]): Unit =
+    {
+        val cdPattern = "\\$ cd ([^\\s]+)".r
+        val dirPattern = "dir (\\w+)".r
+        val filePattern = "(\\d+) ([^\\s]+)".r
+
+        val data = io.Source
+            .fromResource("input.txt")
+            .getLines
+            .toList
+            .map(_ match {
+                case cdPattern(to) => Some(ChangeDirectory(to))
+                case filePattern(len, name) => Some(File(name, len.toInt))
+                case _ => None
+            })
+            .filter(_.isDefined)
+            .map(_.get)
+
+        val dirSizes = calculateDirSizes(data)
+
+        println(dirSizes
+            .toList
+            .map(_._2)
+            .filter(_ <= 100000)
+            .sum)
+    }
+}
