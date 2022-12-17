d3a09bfd8506a139c030d02434b8ef006674ab23
Michał Kullass <mkull@onet.pl>
2022-12-17 19:53:20 +0100

    Day 17 optimized

--- day17/src/main/scala/Puzzle2.scala
+++ day17/src/main/scala/Puzzle2.scala
@@ -9,69 +9,118 @@ object Puzzle2
     def topEdge(rock: Set[(Int, Int)]) = rock.map(_._2).min
     def bottomEdge(rock: Set[(Int, Int)]) = rock.map(_._2).max
 
-    def simulateRocks(rockTypes: Map[Char, Set[(Int, Int)]],
+    case class SimulationState(rock: Set[(Int, Int)], rockStack: Set[(Int, Int)],
+        rockOrderLeft: List[Char], windLeft: List[Char], rockCount: Int,
+        rockStackHeight: Int, rockLeftEdges: Map[Char, Int],
+        rockBottomEdges: Map[Char, Int], stackHeight: Int,
+        rockTypes: Map[Char, Set[(Int, Int)]], rockOrder: List[Char],
+        rockTotalCount: Int, wind: List[Char], width: Int,
+        leftMargin: Int, bottomMargin: Int)
+    
+    def prepareSimulationState(rockTypes: Map[Char, Set[(Int, Int)]],
         rockOrder: List[Char], rockTotalCount: Int,
         wind: List[Char], width: Int, 
         leftMargin: Int, bottomMargin: Int,
-        rockBase: Set[(Int, Int)]): (Set[(Int, Int)], Int) =
+        rockBase: Set[(Int, Int)]): SimulationState =
     {
         val rockLeftEdges = rockTypes.map(t => (t._1, leftEdge(t._2)))
         val rockBottomEdges = rockTypes.map(t => (t._1, bottomEdge(t._2)))
-
-        def spawnRock(rockType: Char, rockStackHeight: Int): Set[(Int, Int)] =
-        {
-            val leftEdge = rockLeftEdges(rockType)
-            val bottomEdge = rockBottomEdges(rockType)
-
-            rockTypes(rockType)
-                .map(t => (t._1 + leftMargin - leftEdge, 
-                    t._2 - rockStackHeight - bottomMargin - bottomEdge - 1))
-        }
-        @tailrec
-        def processRock(rock: Set[(Int, Int)], rockStack: Set[(Int, Int)],
-            rockOrderLeft: List[Char], windLeft: List[Char], rockCount: Int,
-            rockStackHeight: Int): (Set[(Int, Int)], Int) =
-        {
-            if (rockCount > rockTotalCount)
-                (rockStack.filter(t => t._2 < (20 - rockStackHeight)), rockStackHeight)
-            else
-            {
-                windLeft match {
-                    case h :: t => {
-                        val movedRock = rock.map(t => if (h == '<') 
-                            (t._1 - 1, t._2) else (t._1 + 1, t._2))
-                        val validatedRock = if ((leftEdge(movedRock) < 0) || 
-                            (rightEdge(movedRock) >= width) ||
-                            movedRock.exists(rockStack.contains))
-                            rock else movedRock
-                        val fallenRock = validatedRock.map(t => (t._1, t._2 + 1))
-                        if (fallenRock.exists(rockStack.contains)
-                            || (bottomEdge(fallenRock) >= 0))
-                        {
-                            val rol = if (rockOrderLeft.isEmpty) rockOrder else rockOrderLeft
-                            val rockType = rol.head
-                            val newRockStackHeight = Math.max(-topEdge(validatedRock), rockStackHeight)
-                            val newRockStack = rockStack ++ validatedRock
-
-                            processRock(spawnRock(rockType, newRockStackHeight),
-                                newRockStack, rol.tail, t, rockCount + 1, newRockStackHeight)
-                        }
-                        else
-                            processRock(fallenRock, rockStack, rockOrderLeft,
-                                t, rockCount, rockStackHeight)
-                    }
-                    case _ => processRock(rock, rockStack, rockOrderLeft, 
-                            wind, rockCount, rockStackHeight)
-                }
-            }
-        }
-
         val stackHeight = if (rockBase.isEmpty) 0 else -topEdge(rockBase)
-        processRock(spawnRock(rockOrder.head, stackHeight), 
-            rockBase, rockOrder.tail, wind, 1, stackHeight)
+        SimulationState(
+            spawnRock(rockOrder.head, stackHeight, rockLeftEdges, 
+                rockBottomEdges, rockTypes, leftMargin, bottomMargin), 
+                rockBase, rockOrder.tail, wind, 1, stackHeight,
+                rockLeftEdges, rockBottomEdges, stackHeight,
+                rockTypes, rockOrder, rockTotalCount, wind, width, 
+                leftMargin, bottomMargin)
     }
 
-    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    def spawnRock(rockType: Char, rockStackHeight: Int,
+        rockLeftEdges: Map[Char, Int],
+        rockBottomEdges: Map[Char, Int],
+        rockTypes: Map[Char, Set[(Int, Int)]],
+        leftMargin: Int, 
+        bottomMargin: Int): Set[(Int, Int)] =
+    {
+        val leftEdge = rockLeftEdges(rockType)
+        val bottomEdge = rockBottomEdges(rockType)
+
+        rockTypes(rockType)
+            .map(t => (t._1 + leftMargin - leftEdge, 
+                t._2 - rockStackHeight - bottomMargin - bottomEdge - 1))
+    }
+    @tailrec
+    def processRocks(state: SimulationState): (Set[(Int, Int)], Int, SimulationState) =
+    {
+        val rock = state.rock
+        val rockStack = state.rockStack
+        val rockOrderLeft = state.rockOrderLeft
+        val windLeft = state.windLeft
+        val rockCount = state.rockCount
+        val rockStackHeight = state.rockStackHeight
+
+        val rockLeftEdges = state.rockLeftEdges
+        val rockBottomEdges = state.rockBottomEdges
+        val stackHeight = state.stackHeight
+
+        val rockTypes = state.rockTypes
+        val rockOrder = state.rockOrder
+        val rockTotalCount = state.rockTotalCount
+        val wind = state.wind
+        val width = state.width
+        val leftMargin = state.leftMargin
+        val bottomMargin = state.bottomMargin
+
+        if (rockCount > rockTotalCount)
+            (rockStack.filter(t => t._2 < (20 - rockStackHeight)), rockStackHeight, state)
+        else
+        {
+            windLeft match {
+                case h :: t => {
+                    val movedRock = rock.map(t => if (h == '<') 
+                        (t._1 - 1, t._2) else (t._1 + 1, t._2))
+                    val validatedRock = if ((leftEdge(movedRock) < 0) || 
+                        (rightEdge(movedRock) >= width) ||
+                        movedRock.exists(rockStack.contains))
+                        rock else movedRock
+                    val fallenRock = validatedRock.map(t => (t._1, t._2 + 1))
+                    if (fallenRock.exists(rockStack.contains)
+                        || (bottomEdge(fallenRock) >= 0))
+                    {
+                        val rol = if (rockOrderLeft.isEmpty) rockOrder else rockOrderLeft
+                        val rockType = rol.head
+                        val newRockStackHeight = Math.max(-topEdge(validatedRock), rockStackHeight)
+                        val newRockStack = rockStack ++ validatedRock
+
+                        processRocks(SimulationState(
+                            spawnRock(rockType, newRockStackHeight,
+                                rockLeftEdges, rockBottomEdges, rockTypes, 
+                                leftMargin, bottomMargin),
+                            newRockStack, rol.tail, t, rockCount + 1, newRockStackHeight,
+                            rockLeftEdges, rockBottomEdges, 
+                            stackHeight, rockTypes, rockOrder, 
+                            rockTotalCount, wind, width, 
+                            leftMargin, bottomMargin))
+                    }
+                    else
+                        processRocks(SimulationState(
+                            fallenRock, rockStack, rockOrderLeft,
+                            t, rockCount, rockStackHeight,
+                            rockLeftEdges, rockBottomEdges, 
+                            stackHeight, rockTypes, rockOrder, 
+                            rockTotalCount, wind, width, 
+                            leftMargin, bottomMargin))
+                }
+                case _ => processRocks(SimulationState(
+                            rock, rockStack, rockOrderLeft, 
+                            wind, rockCount, rockStackHeight,
+                            rockLeftEdges, rockBottomEdges, 
+                            stackHeight, rockTypes, rockOrder, 
+                            rockTotalCount, wind, width, leftMargin, 
+                            bottomMargin))
+            }
+        }
+    }
 
     def main(args: Array[String]): Unit =
     {
@@ -92,17 +141,54 @@ object Puzzle2
             .getLines
             .toList
             .flatMap(_.toList)
+        
+        @tailrec
+        def searchForMagicNumber(num: Int, state1: SimulationState, state2: SimulationState): Int =
+        {
+            def normalizedEquals(s1: Set[(Int, Int)], s2: Set[(Int, Int)]): Boolean =
+            {
+                val s1xOff = s1.map(_._1).min
+                val s1yOff = s1.map(_._2).min
+                val s2xOff = s2.map(_._1).min
+                val s2yOff = s2.map(_._2).min
 
-        val cycleLength = 1700 // magic number found via input analysis; 35 for test data
+                s1.map(t => (t._1 - s1xOff, t._2 - s1yOff)) ==
+                s2.map(t => (t._1 - s2xOff, t._2 - s2yOff)) 
+            }
+
+            val st1 = state1.copy(rockTotalCount = num)
+            val st2 = state2.copy(rockTotalCount = 2 * num)
+
+            val res1 = processRocks(st1)
+            val res2 = processRocks(st2)
+
+            if (normalizedEquals(res1._1, res2._1))
+                num
+            else
+                searchForMagicNumber(num + rockOrder.size, res1._3, res2._3)
+        }
+        def serchForCycleLength(): Int =
+        {
+            val num = rockOrder.size
+
+            val state1 = prepareSimulationState(rockTypes, rockOrder, num,
+                wind, width, leftMargin, bottomMargin, Set())
+            val state2 = prepareSimulationState(rockTypes, rockOrder, 2 * num,
+                wind, width, leftMargin, bottomMargin, Set())
+
+            searchForMagicNumber(num, state1, state2)
+        }
+
+        val cycleLength = serchForCycleLength()
         val cycleCount = rockCount / cycleLength
         val partialCycleLength = (rockCount % cycleLength).toInt
 
-        val cRes = simulateRocks(rockTypes, rockOrder, cycleLength,
-            wind, width, leftMargin, bottomMargin, Set())
-        val c1Res = simulateRocks(rockTypes, rockOrder, 2 * cycleLength,
-            wind, width, leftMargin, bottomMargin, Set())
-        val pRes = simulateRocks(rockTypes, rockOrder, (2 * cycleLength) + partialCycleLength,
-            wind, width, leftMargin, bottomMargin, Set())
+        val cRes = processRocks(prepareSimulationState(rockTypes, rockOrder, cycleLength,
+            wind, width, leftMargin, bottomMargin, Set()))
+        val c1Res = processRocks(prepareSimulationState(rockTypes, rockOrder, 2 * cycleLength,
+            wind, width, leftMargin, bottomMargin, Set()))
+        val pRes = processRocks(prepareSimulationState(rockTypes, rockOrder, (2 * cycleLength) + partialCycleLength,
+            wind, width, leftMargin, bottomMargin, Set()))
         
         println((cycleCount - 2) * (c1Res._2 - cRes._2) + pRes._2)
     }
